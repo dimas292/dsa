e7200a9ea5b009b49a5189eb124527a41b735eeb
dimas <dimasixc22@gmail.com>
2024-10-11 20:23:32 +0700

    func

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const number = [1, 2, 3, 4, 5]
+
+const getElement = (arr, index) => {
+	return arr[index]
+} 
+
+console.log(getElement(number, 3))
+
+const sayHell0 = (name) => `hello ${name}`
+
+console.log(sayHell0("dimas"))
